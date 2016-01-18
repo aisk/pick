38bab1f33ff03936906435d5458765493e4c2c1c
wong2 <wonderfuly@gmail.com>
2016-01-18 23:12:58 +0800

    fix setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def fread(fname):
 
 setup(
     name='pick',
-    version='0.3.0',
+    version='0.3.1',
     description='pick an option in the terminal with a simple GUI',
     long_description=fread('README.md'),
     keywords='terminal gui',
@@ -18,7 +18,7 @@ setup(
     author='wong2',
     author_email='wonderfuly@gmail.com',
     license='MIT',
-    py_modules=['pick'],
+    packages=['pick'],
     tests_require=['nose'],
     classifiers=[
         'Development Status :: 3 - Alpha',
