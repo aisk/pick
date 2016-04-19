079cf518326d355ba3b7a38b2d538f097134b6d4
wong2 <wonderfuly@gmail.com>
2016-04-19 10:45:34 +0800

    Add example for register_custom_handler

---
+++ example/custom.py
@@ -0,0 +1,17 @@
+#-*-coding:utf-8-*-
+
+from __future__ import print_function
+
+import curses
+from pick import Picker
+
+def go_back(picker):
+    return (None, -1)
+
+title = 'Please choose your favorite programming language: '
+options = ['Java', 'JavaScript', 'Python', 'PHP', 'C++', 'Erlang', 'Haskell']
+
+picker = Picker(options, title)
+picker.register_custom_handler(curses.KEY_LEFT, go_back)
+option, index = picker.start()
+print(option, index)
