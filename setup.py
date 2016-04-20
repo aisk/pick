bcc7f1b6bc6b2ac438e8d2ee20985b3f629817bd
wong2 <wonderfuly@gmail.com>
2016-04-20 11:04:11 +0800

    bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def fread(fname):
 
 setup(
     name='pick',
-    version='0.5.0',
+    version='0.6.0',
     description='pick an option in the terminal with a simple GUI',
     long_description=fread('README.md'),
     keywords='terminal gui',
