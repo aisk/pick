be3657b430ae969031102173a037f98b5612aafe
Pfister, Andreas (GE Power) <andreas.pfister@ge.com>
2017-03-01 21:29:21 +0100

    Initial version of multi select feature, keeping the original pick functionality for compatibility

--- pick/__init__.py
+++ pick/__init__.py
@@ -9,18 +9,19 @@ __all__ = ['Picker', 'pick']
 KEYS_ENTER = (curses.KEY_ENTER, ord('\n'), ord('\r'))
 KEYS_UP = (curses.KEY_UP, ord('k'))
 KEYS_DOWN = (curses.KEY_DOWN, ord('j'))
-
+KEYS_SELECT = (ord(' '), ' ')
 
 class Picker(object):
     """The :class:`Picker <Picker>` object
 
     :param options: a list of options to choose from
     :param title: (optional) a title above options list
+    :param multi_select: (optional) if true its possible to select multiple values by hitting SPACE, defaults to False
     :param indicator: (optional) custom the selection indicator
     :param default_index: (optional) set this if the default selected option is not the first one
     """
 
-    def __init__(self, options, title=None, indicator='*', default_index=0):
+    def __init__(self, options, title=None, multi_select=False, indicator='*', default_index=0):
 
         if len(options) == 0:
             raise ValueError('options should not be an empty list')
@@ -28,6 +29,8 @@ class Picker(object):
         self.options = options
         self.title = title
         self.indicator = indicator
+        self.multi_select = multi_select
+        self.all_selected = []
 
         if default_index >= len(options):
             raise ValueError('default_index should be less than the length of options')
@@ -48,10 +51,24 @@ class Picker(object):
         if self.index >= len(self.options):
             self.index = 0
 
+    def mark_index(self):
+        if self.multi_select:
+            if self.index in self.all_selected:
+                self.all_selected.remove(self.index)
+            else:
+                self.all_selected.append(self.index)
+
     def get_selected(self):
         """return the current selected option as a tuple: (option, index)
+           or as a list of tuples (in case multi_select==True)
         """
-        return self.options[self.index], self.index
+        if self.multi_select:
+            return_tuples = []
+            for selected in self.all_selected:
+                return_tuples.append((self.options[selected], selected))
+            return return_tuples
+        else:
+            return self.options[self.index], self.index
 
     def get_title_lines(self):
         if self.title:
@@ -60,13 +77,20 @@ class Picker(object):
 
     def get_option_lines(self):
         lines = []
-
         for index, option in enumerate(self.options):
             if index == self.index:
                 prefix = self.indicator
             else:
                 prefix = len(self.indicator) * ' '
-            line = '{0} {1}'.format(prefix, option)
+
+            if self.multi_select:
+                if index in self.all_selected:
+                    format = curses.color_pair(1)
+                else:
+                    format = 0
+                line = ('{0} {1}'.format(prefix, option),format)
+            else:
+                line = '{0} {1}'.format(prefix, option)
             lines.append(line)
 
         return lines
@@ -99,7 +123,10 @@ class Picker(object):
         lines_to_draw = lines[scroll_top:scroll_top+max_rows]
 
         for line in lines_to_draw:
-            self.screen.addnstr(y, x, line, max_x-2)
+            if type(line) is tuple:
+                self.screen.addnstr(y, x, line[0], max_x-2, line[1])
+            else:
+                self.screen.addnstr(y, x, line, max_x-2)
             y += 1
 
         self.screen.refresh()
@@ -114,6 +141,8 @@ class Picker(object):
                 self.move_down()
             elif c in KEYS_ENTER:
                 return self.get_selected()
+            elif c in KEYS_SELECT and self.multi_select:
+                self.mark_index()
             elif c in self.custom_handlers:
                 ret = self.custom_handlers[c](self)
                 if ret:
@@ -124,6 +153,8 @@ class Picker(object):
         curses.use_default_colors()
         # hide the cursor
         curses.curs_set(0)
+        #add some color for multi_select
+        curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_WHITE)
 
     def _start(self, screen):
         self.screen = screen
@@ -134,7 +165,7 @@ class Picker(object):
         return curses.wrapper(self._start)
 
 
-def pick(options, title=None, indicator='*', default_index=0):
+def pick(options, title=None, multi_select=False, indicator='*', default_index=0):
     """Construct and start a :class:`Picker <Picker>`.
 
     Usage::
@@ -144,5 +175,5 @@ def pick(options, title=None, indicator='*', default_index=0):
       >>> options = ['option1', 'option2', 'option3']
       >>> option, index = pick(options, title)
     """
-    picker = Picker(options, title, indicator, default_index)
+    picker = Picker(options, title, multi_select, indicator, default_index)
     return picker.start()
