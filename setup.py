aae74ba31e881c1ab7de07251141c22526a61574
Remi Rampin <remirampin@gmail.com>
2019-01-01 07:49:58 -0500

    Convert README to RST for PyPI (#23)
    * Convert README to RST for PyPI
    
    * Add readme_renderer
    
    * Don't install readme_renderer on Python 2.6

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     name='pick',
     version='0.6.4',
     description='pick an option in the terminal with a simple GUI',
-    long_description=fread('README.md'),
+    long_description=fread('README.rst'),
     keywords='terminal gui',
     url='https://github.com/wong2/pick',
     author='wong2',
