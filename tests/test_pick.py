e87b92925f4f30b52a1c2ebdbb29caa32917e1b5
Sebastian Pipping <sebastian@pipping.org>
2020-08-15 16:47:54 +0200

    tests: Stop using deprecated "multi_select" (with underline)
    Was renamed in a1353e2a3ef9ffb4362c876f09dae8123b1eb11c

--- tests/test_pick.py
+++ tests/test_pick.py
@@ -45,7 +45,7 @@ class TestPick(unittest.TestCase):
     def test_multi_select(self):
         title = 'Please choose an option: '
         options = ['option1', 'option2', 'option3']
-        picker = Picker(options, title, multi_select=True, min_selection_count=1)
+        picker = Picker(options, title, multiselect=True, min_selection_count=1)
         assert picker.get_selected() == []
         picker.mark_index()
         assert picker.get_selected() == [('option1', 0)]
