a1f37bf53877d170f976d5d4d0a73098129568e1
Sebastian Pipping <sebastian@pipping.org>
2020-08-16 15:00:09 +0200

    setup.py: Bumop version to 1.0.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def fread(fname):
 
 setup(
     name='pick',
-    version='0.6.7',
+    version='1.0.0',
     description='pick an option in the terminal with a simple GUI',
     long_description=fread('README.rst'),
     keywords='terminal gui',
