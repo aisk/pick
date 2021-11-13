3345fdc0458c0c6e6a8608db0c584d1978c46435
Max <5477952+MaxG87@users.noreply.github.com>
2021-11-13 18:26:41 +0100

    Introduce Type Hints

--- example/custom.py
+++ example/custom.py
@@ -1,5 +1,6 @@
 import curses
 from pick import Picker
+from typing import Tuple
 
 def go_back(picker):
     return (None, -1)
@@ -7,7 +8,7 @@ def go_back(picker):
 title = 'Please choose your favorite programming language: '
 options = ['Java', 'JavaScript', 'Python', 'PHP', 'C++', 'Erlang', 'Haskell']
 
-picker = Picker(options, title)
+picker: Picker[Tuple[None, int]] = Picker(options, title)
 picker.register_custom_handler(curses.KEY_LEFT, go_back)
 option, index = picker.start()
 print(option, index)
