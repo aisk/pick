acdb3335e839a642b3e3855119f3b21768d62b10
Max <5477952+MaxG87@users.noreply.github.com>
2021-11-13 21:29:39 +0100

    Run mypy in test stage

--- tests/test_pick.py
+++ tests/test_pick.py
@@ -1,10 +1,12 @@
+from typing import Dict, Optional
+
 from pick import Picker
 
 
 def test_move_up_down():
     title = "Please choose an option: "
     options = ["option1", "option2", "option3"]
-    picker = Picker(options, title)
+    picker: Picker[str, str] = Picker(options, title)
     picker.move_up()
     assert picker.get_selected() == ("option3", 2)
     picker.move_down()
@@ -15,14 +17,14 @@ def test_move_up_down():
 def test_default_index():
     title = "Please choose an option: "
     options = ["option1", "option2", "option3"]
-    picker = Picker(options, title, default_index=1)
+    picker: Picker[str, str] = Picker(options, title, default_index=1)
     assert picker.get_selected() == ("option2", 1)
 
 
 def test_get_lines():
     title = "Please choose an option: "
     options = ["option1", "option2", "option3"]
-    picker = Picker(options, title, indicator="*")
+    picker: Picker[str, str] = Picker(options, title, indicator="*")
     lines, current_line = picker.get_lines()
     assert lines == [title, "", "* option1", "  option2", "  option3"]
     assert current_line == 3
@@ -30,7 +32,7 @@ def test_get_lines():
 
 def test_no_title():
     options = ["option1", "option2", "option3"]
-    picker = Picker(options)
+    picker: Picker[str, str] = Picker(options)
     lines, current_line = picker.get_lines()
     assert current_line == 1
 
@@ -38,7 +40,7 @@ def test_no_title():
 def test_multi_select():
     title = "Please choose an option: "
     options = ["option1", "option2", "option3"]
-    picker = Picker(options, title, multiselect=True, min_selection_count=1)
+    picker: Picker[str, str] = Picker(options, title, multiselect=True, min_selection_count=1)
     assert picker.get_selected() == []
     picker.mark_index()
     assert picker.get_selected() == [("option1", 0)]
@@ -51,10 +53,10 @@ def test_options_map_func():
     title = "Please choose an option: "
     options = [{"label": "option1"}, {"label": "option2"}, {"label": "option3"}]
 
-    def get_label(option):
+    def get_label(option: Dict[str, str]) -> Optional[str]:
         return option.get("label")
 
-    picker = Picker(options, title, indicator="*", options_map_func=get_label)
+    picker: Picker[str, Dict[str, str]] = Picker(options, title, indicator="*", options_map_func=get_label)
     lines, current_line = picker.get_lines()
     assert lines == [title, "", "* option1", "  option2", "  option3"]
     assert picker.get_selected() == ({"label": "option1"}, 0)
