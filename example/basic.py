b00aff6756d9e43f3f7ef3969a6fff96fef98749
Max <5477952+MaxG87@users.noreply.github.com>
2021-12-09 15:24:34 +0100

    Adapt examples for returned lists
    The examples still tried to unpack the returned list. This was not a
    good demonstration of the new, streamlined, return style type.

--- example/basic.py
+++ example/basic.py
@@ -2,5 +2,7 @@ from pick import pick
 
 title = 'Please choose your favorite programming language: '
 options = ['Java', 'JavaScript', 'Python', 'PHP', 'C++', 'Erlang', 'Haskell']
-option, index = pick(options, title, indicator='=>', default_index=2)
+selection = pick(options, title, indicator='=>', default_index=2)
+assert len(selection) == 1
+option, index = selection[0]
 print(option, index)
--- example/custom.py
+++ example/custom.py
@@ -2,6 +2,16 @@ import curses
 from pick import Picker
 from typing import Tuple
 
+
+def print_selection(selection):
+    if isinstance(selection, list):
+        assert len(selection) == 1
+        option, index = selection[0]
+        print(option, index)
+    else:
+        print("KEY_LEFT pressed!")
+
+
 def go_back(picker):
     return (None, -1)
 
@@ -11,11 +21,11 @@ options = ['Java', 'JavaScript', 'Python', 'PHP', 'C++', 'Erlang', 'Haskell']
 # with type annotation
 picker: Picker[Tuple[None, int], str] = Picker(options, title)
 picker.register_custom_handler(curses.KEY_LEFT, go_back)
-option, index = picker.start()
-print(option, index)
+selection = picker.start()
+print_selection(selection)
 
 # with type warning suppression
 unannotated_picker = Picker(options, title)  # type: ignore[var-annotated]
 unannotated_picker.register_custom_handler(curses.KEY_LEFT, go_back)
-option, index = unannotated_picker.start()
-print(option, index)
+selection = picker.start()
+print_selection(selection)
