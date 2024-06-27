b7d083266efd64f522a5a5533b8b2a887d330496
Henri <henri.de.boutray@colibritd.com>
2024-06-27 07:42:27 +0200

    doc: added example and documented option in readme

--- example/basic.py
+++ example/basic.py
@@ -1,6 +1,12 @@
 from pick import pick
 
+KEY_CTRL_C = 3
+KEY_ESCAPE = 27
+QUIT_KEYS = (KEY_CTRL_C, KEY_ESCAPE, ord("q"))
+
 title = "Please choose your favorite programming language: "
 options = ["Java", "JavaScript", "Python", "PHP", "C++", "Erlang", "Haskell"]
-option, index = pick(options, title, indicator="=>", default_index=2)
+option, index = pick(
+    options, title, indicator="=>", default_index=2, quit_keys=QUIT_KEYS
+)
 print(f"You chose {option} at index {index}")
